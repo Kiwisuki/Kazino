bf56c858c64e111f889f46c6633d6d9252117cdc
AistisK22 <aiskai1@ktu.lt>
2022-05-03 22:36:05 +0300

    Few improvements

--- Bandua.js
+++ Bandua.js
@@ -1,6 +1,7 @@
 var img = document.getElementById('shit');
 var im = document.getElementById('ps');
 img.addEventListener('click', suktis, false);
+var array = Array["Ruletė", "Monetos Metimas", "Fortūnos Ratas", "Sprogimas", "Lošimo automatas"];
 function suktis() {
     var deg = 500 + Math.round(Math.random() * 500);
 
@@ -9,4 +10,9 @@ function suktis() {
     im.setAttribute(
         'style', css
     );
+}
+
+funtion AI(){
+    var array = array[Math.floor(Math.random()*array.length)];
+    document.getElementById("demo").innerHTML = array;
 }
